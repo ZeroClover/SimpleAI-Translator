d02ea587b68616faa02e7455498da029bda70732
Zero Clover <zero@root.me>
2026-05-09 04:00:53 +0800

    refactor: remove legacy tauri hotkey config

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -37,9 +37,6 @@ pub struct ProxyConfig {
 pub struct Config {
     pub hotkey: Option<String>,
     pub display_window_hotkey: Option<String>,
-    pub ocr_hotkey: Option<String>,
-    pub writing_hotkey: Option<String>,
-    pub writing_newline_hotkey: Option<String>,
     pub restore_previous_position: Option<bool>,
     pub always_show_icons: Option<bool>,
     pub allow_using_clipboard_when_selected_text_not_available: Option<bool>,
